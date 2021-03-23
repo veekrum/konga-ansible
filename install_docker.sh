c2a0f5a23f7f2f4185cde1e79a3c393e2791f738
veekrum <veekrum.thapa@gmail.com>
2021-03-23 17:24:57 +0545

    test

--- install_docker.sh
+++ install_docker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -xe;
-apt -y update && \
-apt -y install apt-transport-https ca-certificates curl software-properties-common && \
+sudo apt -y update && \
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common && \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - && \
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && \
-apt -y update && \
+sudo apt -y update && \
 apt-cache policy docker-ce && \
-apt install -y docker-ce && \
+sudo apt install -y docker-ce && \
 curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
-chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
