59b9b493a40532f54518ab6f0aca23408e23cfab
veekrum <veekrum.thapa@gmail.com>
2021-03-23 11:00:09 +0545

    bash scripts and ansible script for installing docker and kong

---
+++ install_kong.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -xe;
+
+#mkdir test && cd test && \
+git clone https://github.com/abrahamjoc/docker-compose-kong-konga.git && \
+cd docker-compose-kong-konga && \
+sed -i 's/1337:/8080:/' docker-compose.yml && \
+sed -i 's/8000:/80:/' docker-compose.yml && \
+sed -i 's/8443:/443:/' docker-compose.yml && \
+
+sudo docker-compose up -d
