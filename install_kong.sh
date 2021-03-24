4a1b00d7f52f62e96da366ba158d3e067861c68c
Bikram Chidi <veekrum.thapa@gmail.com>
2021-03-24 16:54:53 +0545

    Update install_kong.sh
    update

--- install_kong.sh
+++ install_kong.sh
@@ -2,7 +2,7 @@
 set -xe;
 
 #mkdir test && cd test && \
-git clone https://github.com/abrahamjoc/docker-compose-kong-konga.git && \
+git clone https://github.com/abrahamjoc/docker-compose-kong-konga.git && \ ## thanks to abrahamjoc
 cd docker-compose-kong-konga && \
 sed -i 's/1337:/8080:/' docker-compose.yml && \
 sed -i 's/8000:/80:/' docker-compose.yml && \
